bf10af4dc045056ebc88edcd3e25085cf087632a
Ruslam Mir <rmir212121@gmail.com>
2023-02-24 21:50:45 +0300

    func def

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,3 +1,22 @@
+from os import path
+import numpy as np
+from PIL import Image
+
+def openImage(source: str) -> np.array:
+        image = Image.open(source).convert('RGB')
+        return np.array(image)
+
+def interpolate(image):
+    pass
+
+def decimate(image):
+    pass
+
+def twoPassResampling(image):
+    pass
+
+def onePassResampling(image):
+    pass
 
 
 
