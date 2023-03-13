6b1e5f97785e3f3edc5fd3bca397b75b6755f966
Ruslam Mir <rmir212121@gmail.com>
2023-03-13 14:18:15 +0300

    Redone int/dec

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -3,38 +3,42 @@ import numpy as np
 from PIL import Image
 
 def openImage(source: str) -> np.array:
-        image = Image.open(source).convert('RGB')
+        image = Image.open(source)
         return np.array(image)
 
-def interpolate(image: np.array, factor: int):
-    dim_original = image.shape[0:2]
+def interpolate(image, factor: int):
+    data = image.load()
+    dim_original = image.size
     dim_resulting = tuple(d * 2 for d in dim_original)
+    result = Image.new('RGB', dim_resulting)
+    new_data = result.load()
 
-    result = np.empty((*dim_resulting, 3))
 
-
-    for x in range(dim_resulting[0]):
-        for y in range(dim_resulting[1]):
-            result[x, y] = image[min(int(round(x / factor)), dim_original[0] - 1), min(int(round(y / factor)), dim_original[1] - 1)]
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            for offsetx in range(factor):
+                for offsety in range(factor):
+                    new_data[x * factor + offsetx, y * factor + offsety] = data[x, y]
     return result
 
-def decimate(image: np.array, factor: int):
-    dim_original = image.shape[0:2]
+def decimate(image, factor: int):
+    data = image.load()
+
+    dim_original = image.size
     dim_resulting = tuple(int(d / 2) for d in dim_original)
 
-    result = np.empty((*dim_resulting, 3))
+    result = Image.new('RGB', dim_resulting)
+    new_data = result.load()
 
-    for x in range(dim_resulting[0]):
-        for y in range(dim_resulting[1]):
-            result[x, y] = image[
-                min(int(round(x * factor)), dim_original[0] - 1), min(int(round(y * factor)), dim_original[1] - 1)]
+    for x in range(result.size[0]):
+        for y in range(result.size[1]):
+            new_data[x, y] = data[x * factor, y * factor]
     return result
 
-def twoPassResampling(image: np.array, ifactor: int, dfactor: int):
-    temp = interpolate(image, ifactor)
-    return decimate(temp, dfactor)
+def twoPassResampling(image, ifactor: int, dfactor: int):
+    return decimate(interpolate(image, ifactor), dfactor)
 
-def onePassResampling(image: np.array, factor: float):
+def onePassResampling(image: np.array, factor: float) -> np.array:
     dim_original = image.shape[0:2]
     dim_resulting = tuple(int(d * 2) for d in dim_original)
 
@@ -51,26 +55,23 @@ def onePassResampling(image: np.array, factor: float):
 
 
 if __name__ == '__main__':
-    imgn = 2
-    img = openImage(f'./srcimg/sample{imgn}.png')
-    new_img = interpolate(img, 2)
-    res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
-    res.save(f'./resultimg/sample{imgn}.png')
+    srcpath = ''
+    resultpath = ''
 
-    imgn = 1
-    img = openImage(f'./srcimg/sample{imgn}.png')
-    new_img = decimate(img, 2)
-    res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
-    res.save(f'./resultimg/sample{imgn}.png')
+    img = Image.open(f'./srcimg/sample1.png').convert('RGB')
+    interpolate(img, 2).save(f'./resultimg/sample1.png')
 
-    imgn = 1
-    img = openImage(f'./srcimg/sample{imgn}.png')
-    new_img = onePassResampling(img, 1.4)
-    res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
-    res.save(f'./resultimg/sample{3}.png')
+    img = Image.open(f'./srcimg/sample1.png').convert('RGB')
+    decimate(img, 2).save(f'./resultimg/sample2.png')
 
-    imgn = 2
-    img = openImage(f'./srcimg/sample{imgn}.png')
-    new_img = twoPassResampling(img, 4, 7)
-    res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
-    res.save(f'./resultimg/sample{4}.png')
+
+    # img = openImage(f'./srcimg/sample{imgn}.png')
+    # new_img = onePassResampling(img, 1.4)
+    # res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
+    # res.save(f'./resultimg/sample{3}.png')
+    #
+    # imgn = 2
+    # img = openImage(f'./srcimg/sample{imgn}.png')
+    # new_img = twoPassResampling(img, 4, 7)
+    # res = Image.fromarray(new_img.astype(np.uint8), 'RGB')
+    # res.save(f'./resultimg/sample{4}.png')
