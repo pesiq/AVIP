bd44e0524b17c226611ebede4ad31f64456d527d
Ruslan Mirziianov <rmir212121@gmail.com>
2023-08-19 14:05:26 +0300

    lab4 start

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -1,4 +1,14 @@
 import numpy as np
 from PIL import Image
 
+def grad_matrix(arg):
+    
 
+
+
+if __name__ == "__main__":
+
+    srcpath = ''
+
+    with Image.open(srcpath).convert('1') as img:
+       pass 
